e2f9b8b31bc2dc070801db9cbe7b5eb719ffb9ba
Victor Barros <victorbarros1130@gmail.com>
2020-08-04 23:26:22 -0300

    Add css, assets and itegrationg with backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api'
+
+import './App.css';
+import backgroundImage from './assets/background.jpeg'
 
 import Header from './components/Header';
 
 function App() {
-    const [projects, setProjects] = useState(['front', 'devs']);
+    const [projects, setProjects] = useState([]);
     // useState retorna array com duas posições
     // Na primeira retorna o valor inicial
     // No segundo retorna uma função para atualizar valor
 
+    useEffect(() => {
+        api.get('/projects').then(response => {
+            // console.log(response);
+            setProjects(response.data);
+        });
+    }, []);
     function handleAddProject() {
         // projects.push(`New Project ${Date.now()}`);
         setProjects([...projects, `New Project ${Date.now()}`]); // ... spread operator
@@ -22,11 +32,12 @@ function App() {
                     <li>gre</li>
                 </ul>
             </Header>
+            {/* <img width={350} src={backgroundImage}/> */}
 
             <h1>Estado</h1>
             <p>
                 <ul>
-                    {projects.map(project => <li key={project}>{project}</li>)}
+                    {projects.map(project => <li key={project.id}>{project.title}</li>)}
                 </ul>
                 <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
             </p>
