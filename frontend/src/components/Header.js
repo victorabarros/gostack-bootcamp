8fee360695906dc5192e5f00530b828dc91067be
Victor Barros <victorbarros1130@gmail.com>
2020-08-04 21:22:14 -0300

    LEarning components, fragments and mor about react

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+    // Component: function que returna html. Jump of the Cat no react.
+    return (
+        <header>
+            <h1>ReactJS</h1>
+        </header>
+    );
+};
